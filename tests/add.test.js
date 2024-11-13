aa80ab9244a928f6acc515787a3c43fa2ed9e43b
epltnn <eemeli.peltonen@tuni.fi>
2024-11-13 15:13:13 +0200

    some updates

--- tests/add.test.js
+++ tests/add.test.js
@@ -1,10 +1,11 @@
 import add from '../src/add.js';
 import *  as chai from 'chai';
+import 'mocha';
 
 const expect = chai.expect;
 
 describe('Add test1', () => {
     it('1 + 1 should equal 2', () => {
-    expect(add(1, 2)).to.equal(2);
+    expect(add(1, 1)).to.equal(2);
     });
 });
